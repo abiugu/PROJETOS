bdf5dd10d8efb148cea4ae81ffeaeca4e1ac74bd
abiugu <abiugu@gmail.com>
2024-10-15 02:44:22 -0300

    i

--- Automação de entradas/alerta 100.py
+++ Automação de entradas/alerta 100.py
@@ -16,7 +16,7 @@ service = Service()
 
 # Configurando as opções do Chrome
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")  # Executar em modo headless
+options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
